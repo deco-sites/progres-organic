b7a9a8011e24c39fb5b5bc36b820143a7e6a961c
Karla Oshikawa <karla_hara@hotmail.com>
2024-10-11 02:56:34 +0900

    feat: konfidency

--- sections/ReviewsPDP.tsx
+++ sections/ReviewsPDP.tsx
@@ -1,110 +1,71 @@
-import { ProductDetailsPage } from "apps/commerce/types.ts";
+// import { ProductDetailsPage } from "apps/commerce/types.ts";
 
-export interface Props {
-  /** @title Integration */
-  page: ProductDetailsPage | null;
-}
+// export interface Props {
+//   /** @title Integration */
+//   page: ProductDetailsPage | null;
+// }
 
-const mockReview = {
-  reviews: [
-    {
-      _id: "337892",
-      aggregateRating: 4.6,
-      recommendedPercentage: 100,
-      reviewCount: 13,
-      reviews: [
-        {
-          _id: "612a928939ff8f181675bae8",
-          customer: "tokstok",
-          sku: "337892",
-          name: "Bruna Hellen dos Santos Renalvo",
-          rating: 5,
-          text: "Simplesmente encantada com o resultado.",
-          helpful: 4,
-          unhelpful: 0,
-          verified: false,
-          created: "2020-08-30T11:32:11.000Z",
-          status: "published",
-          recommended: true,
-          updated: "2021-12-01T23:08:09.432Z",
-        },
-        {
-          _id: "612a928939ff8f181675bae9",
-          customer: "johndoe",
-          sku: "337892",
-          name: "Elisalda de De Melo Miranda",
-          rating: 3,
-          text: "Ótimo produto,.eu cabelo está crescendo muito.",
-          helpful: 2,
-          unhelpful: 1,
-          verified: true,
-          created: "2021-01-15T10:45:30.000Z",
-          status: "published",
-          recommended: true,
-          updated: "2021-12-02T15:12:00.432Z",
-        },
-      ],
-    },
-  ],
-};
+// const mockReview = {
+//   reviews: [
+//     {
+//       _id: "337892",
+//       aggregateRating: 4.6,
+//       recommendedPercentage: 100,
+//       reviewCount: 13,
+//       reviews: [
+//         {
+//           _id: "612a928939ff8f181675bae8",
+//           customer: "tokstok",
+//           sku: "337892",
+//           name: "Bruna Hellen dos Santos Renalvo",
+//           rating: 5,
+//           text: "Simplesmente encantada com o resultado.",
+//           helpful: 4,
+//           unhelpful: 0,
+//           verified: false,
+//           created: "2020-08-30T11:32:11.000Z",
+//           status: "published",
+//           recommended: true,
+//           updated: "2021-12-01T23:08:09.432Z",
+//         },
+//         {
+//           _id: "612a928939ff8f181675bae9",
+//           customer: "johndoe",
+//           sku: "337892",
+//           name: "Elisalda de De Melo Miranda",
+//           rating: 3,
+//           text: "Ótimo produto,.eu cabelo está crescendo muito.",
+//           helpful: 2,
+//           unhelpful: 1,
+//           verified: true,
+//           created: "2021-01-15T10:45:30.000Z",
+//           status: "published",
+//           recommended: true,
+//           updated: "2021-12-02T15:12:00.432Z",
+//         },
+//       ],
+//     },
+//   ],
+// };
 
-export default function Section({ page }: Props) {
-  if (!page || !page.product) return null;
+export default function ReviewPDP() {
+  // if (!page || !page.product) return null;
 
-  function formatDate(dateString: string) {
-    const date = new Date(dateString);
-    return date.toLocaleDateString("pt-BR");
-  }
+  // function formatDate(dateString: string) {
+  //   const date = new Date(dateString);
+  //   return date.toLocaleDateString("pt-BR");
+  // }
 
   return (
     <div class="hidden md:flex  xl:px-[132px] md:w-[1440px]  mx-auto text-primary pt-10 mb-8">
-      {page.product.review
-        ? (
-          <div>
-            <script
-              async
-              src="https://reviews.konfidency.com.br/progressivaorganica/loader.js"
-            />
-            <div class="konfidency-reviews-details"></div>
-          </div>
-        )
-        : (
-          <div class="mx-auto">
-            <div class="flex justify-between mb-[29px]">
-              <div class="flex gap-10 items-start">
-                <div class="border-b-2 border-primary leading-1 p-2">
-                  <span>Avaliações</span>
-                </div>
-                <div class="leading-1 h-[42px] flex items-center">
-                  <span>Perguntas</span>
-                </div>
-              </div>
-              <div class="flex gap-5 ">
-                <div class="btn btn-outline btn-primary w-[127px] h-[42px] min-h-0">
-                  <span>Avaliar</span>
-                </div>
-                <div class="btn btn-outline btn-primary w-[127px] h-[42px]  min-h-0">
-                  <span>Perguntar</span>
-                </div>
-              </div>
-            </div>
-            <div class="flex flex-col xl:flex-row gap-6 ">
-              {mockReview.reviews.map((review) =>
-                review.reviews.map((item) => (
-                  <div class="border border-primary h-[250px] w-[576px] min-w-[300px] p-3 rounded">
-                    <p class="text-[12px] h-[51px] w-full text-end">
-                      {formatDate(item.created)}
-                    </p>
-                    <p class="text-sm h-[148px]">{item.text}</p>
-                    <p class="text-base font-bold text-center h-[51px]">
-                      {item.name}
-                    </p>
-                  </div>
-                ))
-              )}
-            </div>
-          </div>
-        )}
+      <div>
+        <div class="konfidency-reviews-details"></div>
+      </div>
+
+      <script
+        async
+        src="https://reviews.konfidency.com.br/progressivaorganica/loader.js"
+      />
     </div>
   );
 }
